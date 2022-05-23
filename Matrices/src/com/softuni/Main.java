576dd618b3c95b08b97c2e8ac2af8850214ca231
DimoHG <dimo.georgiev@outlook.com>
2022-05-23 22:01:41 +0300

    Matrices

--- Matrices/src/com/softuni/Main.java
+++ Matrices/src/com/softuni/Main.java
@@ -1,8 +1,32 @@
 package com.softuni;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int rows = 3;
+        int[][] matrix = new int[rows][];
+
+        for (int row = 0; row < rows; row++) {
+            int[] rowInput = Arrays.stream(scanner.nextLine().split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            matrix[row] = rowInput;
+        }
+
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                System.out.print(matrix[row][col] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        //1 2 3
+        //4 5 6
+
+
     }
 }
---
+++ Matrices/src/com/softuni/Test.java
@@ -0,0 +1,7 @@
+package com.softuni;
+
+public class Test {
+    public static void main(String[] args) {
+        int num = 2;
+    }
+}
