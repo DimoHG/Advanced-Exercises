fd0d90aff967e8b705453ae1aef50d6e60e65e33
DimoHG <dimo.georgiev@outlook.com>
2022-07-04 21:51:25 +0300

    Encapsulation

---
+++ EncapsulationExercise/src/com/softuni/Main.java
@@ -0,0 +1,15 @@
+package com.softuni;
+
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println();
+        print();
+    }
+
+    public static void print(){
+        System.out.println("hello");
+    }
+}
