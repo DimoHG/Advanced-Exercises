04a69d3c8bcff0c77e1ea71ff76f942a58c18f47
DimoHG <dimo.georgiev@outlook.com>
2022-07-06 21:02:53 +0300

    Inheritance exercise

---
+++ Inheritance/src/com/softuni/Main.java
@@ -0,0 +1,12 @@
+package com.softuni;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Animal animal = new Animal( 25, "Pehso");
+        animal.jump();
+        //IS A Relationships
+        //Dog is an Animal
+        //Car is an Animal
+    }
+}
